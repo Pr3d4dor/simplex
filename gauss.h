e60cecc6a3b5fc8c216608e2b56316ba7a997ce8
Gianluca Bine <gian_bine@hotmail.com>
2017-05-27 02:15:29 -0300

    Update gauss.h

--- gauss.h
+++ gauss.h
@@ -3,7 +3,6 @@
 
 #include <stddef.h>
 
-extern float modulo(float x);
-extern float *gauss(size_t ordem, float matriz[ordem][ordem], float b[ordem]);
+extern float *gauss(size_t ordem, float **matriz, float b[ordem]);
 
 #endif
